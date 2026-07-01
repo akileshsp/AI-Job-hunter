97c3b49134c82336f3b83773559446ca1a1c3286
akileshsp <akileshsp22@gmail.com>
2026-07-01 11:15:44 +0000

    Cleanup project structure

--- app/app/job_sources/base_job_source.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseJobSource(ABC):
-
-    @abstractmethod
-    def search(self, keyword, location):
-        pass
--- app/app/job_sources/greenhouse_api.py
+++
@@ -1,5 +0,0 @@
-app/job_sources/
-    greenhouse_api.py
-    lever_api.py
-    ashby_api.py
-    workday_api.py
